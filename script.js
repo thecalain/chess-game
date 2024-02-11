531d186c13593e0f07ea6e305e730eb3c69fb3e4
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2024-02-11 18:12:22 +0800

    creates a feature where you can play black or white using the orientation applicable to each

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-const squares = document.querySelectorAll('button');
-let firstClick = false;
-squares.forEach((square) => {
-	square.addEventListener('click', () => {
-		if (firstClick === false && square.textContent !== '') {
-			square.style.opacity = 0.5;
-			firstClick = true;
-		} else if (firstClick === true && square.textContent === '') {
-			square.style.opacity = 0.5;
-			setTimeout(() => {
-				squares.forEach((opacityOfSquare) => {
-					opacityOfSquare.style.opacity = 1;
-				});
-				firstClick = false;
-			}, 200);
-		} else if (firstClick === true && square.textContent !== '') {
-			setTimeout(() => {
-				squares.forEach((opacityOfSquare) => {
-					opacityOfSquare.style.opacity = 1;
-				});
-			}, 200);
-			setTimeout(() => {
-				square.style.opacity = 0.5;
-			}, 200);
-			firstClick = true;
-		} else {
-			firstClick = false;
-		}
+let squares = document.querySelectorAll('button');
+const playWhite = document.getElementById('play-white');
+const playBlack = document.getElementById('play-black');
+const board = document.getElementById('board');
+
+playWhite.addEventListener('click', () => {
+	board.style.display = 'grid';
+	playWhite.style.display = 'none';
+	playBlack.style.display = 'none';
+});
+
+playBlack.addEventListener('click', () => {
+	board.style.display = 'grid';
+	playWhite.style.display = 'none';
+	playBlack.style.display = 'none';
+	squaresArray = Array.from(squares);
+	squaresArray.reverse();
+	squaresArray.forEach((square, index) => {
+		square.style.order = index + 1;
 	});
 });
+
+function highlighting() {
+	const squares = document.querySelectorAll('button');
+	let firstClick = false;
+	squares.forEach((square) => {
+		square.addEventListener('click', () => {
+			if (firstClick === false && square.textContent !== '') {
+				square.style.opacity = 0.5;
+				firstClick = true;
+			} else if (firstClick === true && square.textContent === '') {
+				square.style.opacity = 0.5;
+				setTimeout(() => {
+					squares.forEach((opacityOfSquare) => {
+						opacityOfSquare.style.opacity = 1;
+					});
+					firstClick = false;
+				}, 200);
+			} else if (firstClick === true && square.textContent !== '') {
+				setTimeout(() => {
+					squares.forEach((opacityOfSquare) => {
+						opacityOfSquare.style.opacity = 1;
+					});
+				}, 200);
+				setTimeout(() => {
+					square.style.opacity = 0.5;
+				}, 200);
+				firstClick = true;
+			} else {
+				firstClick = false;
+			}
+		});
+	});
+}
