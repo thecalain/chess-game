c2ca96d9ee6c33875ea2c81f8fa3b0bb75314314
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2024-02-13 11:32:17 +0800

    console.log's the piece that was selected 1. Refactors the code so that there is an "Array" class that makes it possible to highlight and select the piece with the "document.getElementsByClassName" was changed due to interactions with the "Play White" and "Play Black" button 2. "Play White" and "Play Black" buttons were removed instead of hidden when clicked

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-let squares = document.querySelectorAll('button');
+const squares = Array.from(document.getElementsByClassName('square'));
 const playWhite = document.getElementById('play-white');
 const playBlack = document.getElementById('play-black');
 const board = document.getElementById('board');
+let gameSetup = true;
+let gameOngoing = false;
+let firstClick = false;
+let pieceSelected;
 
 playWhite.addEventListener('click', () => {
+	playWhite.remove();
+	playBlack.remove();
 	board.style.display = 'grid';
-	playWhite.style.display = 'none';
-	playBlack.style.display = 'none';
+	gameSetup = false;
+	gameOngoing = true;
 });
 
 playBlack.addEventListener('click', () => {
+	playWhite.remove();
+	playBlack.remove();
 	board.style.display = 'grid';
-	playWhite.style.display = 'none';
-	playBlack.style.display = 'none';
 	squaresArray = Array.from(squares);
 	squaresArray.reverse();
 	squaresArray.forEach((square, index) => {
 		square.style.order = index + 1;
 	});
+	gameSetup = false;
+	gameOngoing = true;
 });
 
-function highlighting() {
-	const squares = document.querySelectorAll('button');
-	let firstClick = false;
-	squares.forEach((square) => {
-		square.addEventListener('click', () => {
+squares.forEach((square) => {
+	square.addEventListener('click', () => {
+		if (!gameSetup && gameOngoing) {
 			if (firstClick === false && square.textContent !== '') {
 				square.style.opacity = 0.5;
 				firstClick = true;
+				pieceSelected = square.textContent;
+				console.log(pieceSelected);
 			} else if (firstClick === true && square.textContent === '') {
 				square.style.opacity = 0.5;
 				setTimeout(() => {
@@ -49,6 +57,17 @@ function highlighting() {
 			} else {
 				firstClick = false;
 			}
-		});
+		}
 	});
-}
+});
+
+// function pawnMovement() {
+// 	if (firstClick === true && pieceSelected === '♟') {
+// 		squares.forEach((square) => {
+// 			square.addEventListener('click', () => {
+// 				if ((square.textContent = '♟' && firstClick === true)) {
+// 				}
+// 			});
+// 		});
+// 	}
+// }
