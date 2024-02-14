1351b70cbd97942cd1b994037d3fa1ff7d849fa4
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2024-02-14 17:21:55 +0800

    Adds piece movement but unsucessful at the moment 1. Allow general movement without restriction (ok but adds restriction) 2. Captures the piece landed on but still retains the piece in the original position (must fix but let go for the meantime) 3. Highlighting is inconsistent again (unintended) 4. Refactors and add code that does all of these

--- script.js
+++ script.js
@@ -4,70 +4,77 @@ const playBlack = document.getElementById('play-black');
 const board = document.getElementById('board');
 let gameSetup = true;
 let gameOngoing = false;
-let firstClick = false;
-let pieceSelected;
 
-playWhite.addEventListener('click', () => {
-	playWhite.remove();
-	playBlack.remove();
-	board.style.display = 'grid';
-	gameSetup = false;
-	gameOngoing = true;
-});
-
-playBlack.addEventListener('click', () => {
-	playWhite.remove();
-	playBlack.remove();
-	board.style.display = 'grid';
-	squaresArray = Array.from(squares);
-	squaresArray.reverse();
-	squaresArray.forEach((square, index) => {
-		square.style.order = index + 1;
+function gameStart() {
+	playWhite.addEventListener('click', () => {
+		playWhite.remove();
+		playBlack.remove();
+		board.style.display = 'grid';
+		gameSetup = false;
+		gameOngoing = true;
 	});
-	gameSetup = false;
-	gameOngoing = true;
-});
 
-squares.forEach((square) => {
-	square.addEventListener('click', () => {
-		if (!gameSetup && gameOngoing) {
-			if (firstClick === false && square.textContent !== '') {
-				square.style.opacity = 0.5;
-				firstClick = true;
-				pieceSelected = square.textContent;
-				console.log(pieceSelected);
-			} else if (firstClick === true && square.textContent === '') {
-				square.style.opacity = 0.5;
-				setTimeout(() => {
-					squares.forEach((opacityOfSquare) => {
-						opacityOfSquare.style.opacity = 1;
-					});
-					firstClick = false;
-				}, 200);
-			} else if (firstClick === true && square.textContent !== '') {
-				setTimeout(() => {
-					squares.forEach((opacityOfSquare) => {
-						opacityOfSquare.style.opacity = 1;
-					});
-				}, 200);
-				setTimeout(() => {
+	playBlack.addEventListener('click', () => {
+		playWhite.remove();
+		playBlack.remove();
+		board.style.display = 'grid';
+		squaresArray = Array.from(squares);
+		squaresArray.reverse();
+		squaresArray.forEach((square, index) => {
+			square.style.order = index + 1;
+		});
+		gameSetup = false;
+		gameOngoing = true;
+	});
+}
+
+function generalMovement() {
+	let firstClick = false;
+	let pieceSelected;
+	squares.forEach((square) => {
+		square.addEventListener('click', () => {
+			if (!gameSetup && gameOngoing) {
+				if (!firstClick && square.textContent !== '') {
+					setTimeout(() => {
+						square.style.opacity = 0.5;
+					}, 2000);
+					firstClick = true;
+					pieceSelected = square.textContent;
+					firstClickCoordinates = square.id;
+					console.log(firstClickCoordinates);
+				} else if (firstClick && square.textContent === '') {
 					square.style.opacity = 0.5;
-				}, 200);
-				firstClick = true;
-			} else {
-				firstClick = false;
+					setTimeout(() => {
+						squares.forEach((opacityOfSquare) => {
+							opacityOfSquare.style.opacity = 1;
+						});
+						firstClick = false;
+					}, 200);
+					SecondClickCoordinates = square.id;
+					firstClickedSquare = document.getElementById(
+						firstClickCoordinates
+					);
+					firstClickedSquare.textContent = '';
+					console.log(SecondClickCoordinates);
+				} else if (firstClick && square.textContent !== '') {
+					setTimeout(() => {
+						squares.forEach((opacityOfSquare) => {
+							opacityOfSquare.style.opacity = 1;
+						});
+					}, 200);
+					setTimeout(() => {
+						square.style.opacity = 0.5;
+					}, 200);
+					firstClick = true;
+				} else {
+					firstClick = false;
+				}
 			}
-		}
+			square.textContent = pieceSelected;
+			console.log(pieceSelected);
+		});
 	});
-});
+}
 
-// function pawnMovement() {
-// 	if (firstClick === true && pieceSelected === '♟') {
-// 		squares.forEach((square) => {
-// 			square.addEventListener('click', () => {
-// 				if ((square.textContent = '♟' && firstClick === true)) {
-// 				}
-// 			});
-// 		});
-// 	}
-// }
+gameStart();
+generalMovement();
